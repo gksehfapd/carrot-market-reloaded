be9a2e17fd77156164d7147f92661a3d51adce26
gksehfapd <1ehfapd@gmail.com>
2024-07-25 10:40:42 +0900

    feat: 회원가입 체크

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,39 +1,40 @@
 'use server'
 import { z } from 'zod'
 
-const checkUername = (username: string) => {
-	return !username.includes('potato')
-}
-
-const checkPassword = ({
-	password,
-	confirm_password
-}: {
-	password: string
-	confirm_password: string
-}) => {
-	password === confirm_password
-}
+const passwordRegex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/)
 
 const formSchema = z
 	.object({
 		username: z
 			.string({
-				invalid_type_error: 'Username must be a string',
-				required_error: 'Where is my username?'
+				invalid_type_error: 'Username must be a string!',
+				required_error: 'Where is my username???'
 			})
-			.min(5, 'too short')
-			.max(10, 'too long')
-			.refine(checkUername, 'No potatos allowed!'),
-		email: z.string().email(),
-		password: z.string().min(10),
-		confirm_password: z.string().min(10)
+			.min(3, 'Way too short!!!')
+			//.max(10, "That is too looooong!")
+			.trim()
+			.toLowerCase()
+			.transform((username) => `🔥 ${username}`)
+			.refine((username) => !username.includes('potato'), 'No potatoes allowed!'),
+		email: z.string().email().toLowerCase(),
+		password: z
+			.string()
+			.min(4)
+			.regex(
+				passwordRegex,
+				'Passwords must contain at least one UPPERCASE, lowercase, number and special characters #?!@$%^&*-'
+			),
+		confirm_password: z.string().min(4)
 	})
-	.refine(checkPassword, {
-		message: 'Both passwords should be the same',
-		path: ['confirm_password']
+	.superRefine(({ password, confirm_password }, ctx) => {
+		if (password !== confirm_password) {
+			ctx.addIssue({
+				code: 'custom',
+				message: 'Two passwords should be equal',
+				path: ['confirm_password']
+			})
+		}
 	})
-
 export async function createAccount(prevState: any, formData: FormData) {
 	const data = {
 		username: formData.get('username'),
@@ -42,9 +43,9 @@ export async function createAccount(prevState: any, formData: FormData) {
 		confirm_password: formData.get('confirm_password')
 	}
 	const result = formSchema.safeParse(data)
-
 	if (!result.success) {
-		console.log(result.error.flatten())
 		return result.error.flatten()
+	} else {
+		console.log(result.data)
 	}
 }
